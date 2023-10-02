eb26d9e8cb83216784edbeda4b1ed01ef643ac13
Manan Dhiman <manandheman@gmail.com>
2023-10-02 23:11:35 +0530

    Update AboutFragment.kt

--- app/src/main/java/manandhiman/ny_times_reader_unofficial/fragments/AboutFragment.kt
+++ app/src/main/java/manandhiman/ny_times_reader_unofficial/fragments/AboutFragment.kt
@@ -1,13 +1,11 @@
 package manandhiman.ny_times_reader_unofficial.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import manandhiman.ny_times_reader_unofficial.R
+import androidx.fragment.app.Fragment
 import manandhiman.ny_times_reader_unofficial.databinding.FragmentAboutBinding
-import manandhiman.ny_times_reader_unofficial.databinding.FragmentSearchNewsBinding
 
 class AboutFragment : Fragment() {
   private lateinit var binding: FragmentAboutBinding
